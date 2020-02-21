c1a5b61b91616fa67c52cf73ead32817d350d7ee
Ho-Wan <ho.wan2345@gmail.com>
2020-02-21 16:31:37 +0000

    connect and read form firestore

--- covid-api.go
+++ covid-api.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/firestore"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,15 +21,53 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s!\n", target)
 }
 
+func connectFirestore() {
+	// Sets your Google Cloud Platform project ID.
+	projectID := "sd-covid-2"
+
+	// Get a Firestore client.
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	readFromStore(ctx, client)
+
+	// Close client when done.
+	defer client.Close()
+}
+
+func readFromStore(ctx context.Context, client *firestore.Client) {
+	iter := client.Collection("covid_data").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Printf("Found firestore data at ref: %v", doc.Ref)
+	}
+}
+
 func main() {
 	log.Print("Hello world sample started.")
 
 	http.HandleFunc("/", handler)
 
+	err := os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "./config/sd-covid-2-3c873e023505.json")
+	if err != nil {
+		log.Println("Error", err)
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	connectFirestore()
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-covid-api
 
 go 1.13
+
+require (
+	cloud.google.com/go/firestore v1.1.1
+	google.golang.org/api v0.14.0
+)
