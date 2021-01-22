100ab2e72ef485b4639b8fac74967eda6d86a321
Ho-Wan <ho.wan2345@gmail.com>
2021-01-22 23:31:10 +0000

    allow origin for amplify raw url

--- app/app.go
+++ app/app.go
@@ -26,7 +26,7 @@ func ConnectFirestore() http.Handler {
 	c := cors.New(cors.Options{
 		AllowCredentials: true,
 		AllowedMethods:   []string{"GET"},
-		AllowedOrigins:   []string{"http://localhost:3000", "https://covid-api.spandraw.com", "https://covid.spandraw.com", "https://covid-api-uahbr4oaja-ue.a.run.app"},
+		AllowedOrigins:   []string{"http://localhost:3000", "https://covid-api.spandraw.com", "https://covid.spandraw.com", "https://covid-api-uahbr4oaja-ue.a.run.app", "https://master.d2l7iq418hrozi.amplifyapp.com"},
 		AllowedHeaders:   []string{"Origin", "Content-Length", "Content-Type"},
 		// Change Debug to true to show additional logging information
 		Debug: false,
